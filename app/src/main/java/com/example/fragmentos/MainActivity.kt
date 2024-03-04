c1f19d306c96b2833311794ce607b9ba78f8b8e7
Oscar-I-M-G <oscar.moreno4275@alumnos.udg.mx>
2024-03-04 19:29:32 +0100

    Changed some stuff

--- app/src/main/java/com/example/fragmentos/MainActivity.kt
+++ app/src/main/java/com/example/fragmentos/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.fragmentos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firstFragment = FirstFragment()
+
+        // Get the FragmentContainerView for the first fragment
+        val fragmentContainerView = findViewById<FragmentContainerView>(R.id.fcv1)
+
+        // Replace the fragment in the FragmentContainerView with the FirstFragment
+        supportFragmentManager.beginTransaction()
+            .replace(fragmentContainerView.id, firstFragment)
+            .commit()
     }
 }
